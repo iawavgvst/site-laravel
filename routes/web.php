844c1d5ffc2d66d26c3a6039a5093ebd5221ca88
iawavgvst <alkelin1@gmail.com>
2024-09-24 19:59:38 +0300

    auth+front+admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/index.html', [IndexController::class, '__invoke'])->name('index');
 });
 
 Auth::routes();
 
+Route::get('/post.html', [PostController::class, '__invoke'])->name('posts.index');
+Route::get('/about.html', [AboutController::class, '__invoke'])->name('about.index');
+Route::get('/contact.html', [ContactController::class, '__invoke'])->name('contact.index');
+
+Auth::routes();
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin'], function () {
+    Route::group(['namespace' => 'Post'], function () {
+        Route::get('/post', [PostController::class, '__invoke'])->name('admin.post.index');
+    });
+});
